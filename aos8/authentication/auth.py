49ea1d25ce191dc70dcf1a06369c55b6b2ef43a4
Fisher <EFisher@BainCapital.com>
2018-07-23 08:54:09 -0400

    radius nas ip

--- aos8/authentication/auth.py
+++ aos8/authentication/auth.py
@@ -791,3 +791,77 @@ def post_tacacs_accounting(session, config_path, action, **kwargs):
         result_str = f'POST to \'{session.api_url}{post_url}\' unsuccessful'
         result = {'result_status': 1, 'result_str': result_str} 
         return result
+
+def get_radius_nas_ip(session, config_path):
+    
+    get_url = 'configuration/object/rad_nas_ip'
+
+    if (session.api_verbose == True):
+        print(f'Verbose: Sending GET to \'{session.api_url}{get_url}\' to retrieve RADIUS NAS IP address')
+    
+    response = session.get(get_url, config_path)
+
+    if (response.status_code == 200):
+        response_json = response.json()
+        if (session.api_verbose == True):
+                print('Verbose: RADIUS NAS IP address retrieved successfully')
+        return response_json['_data']['rad_nas_ip']
+    
+    else:
+        if (session.api_verbose == True):
+                print('Verbose: Unable to retrive RADIUS NAS IP address')
+        return None
+
+def post_radius_nas_ip(session, config_path, action, **kwargs):
+    
+    if (action != 'add' and action != 'delete' ):
+        result_str = f'\'{action}\' is not an acceptable API action'
+        result = {'result_status': 1, 'result_str': result_str} 
+        return result
+
+    if (action == 'add'):
+
+        payload = {
+            '_action': 'add'
+        }
+
+        for key, value in kwargs.items():
+            if(key == 'nasip'):
+                payload[key] = value
+                break
+            elif(key == 'nasvlan'):
+                payload[key] = value
+                break
+            else:
+                result_str = f'\'{key}\' is not a configurable setting for RADIUS NAS IP address'
+                result = {'result_status': 1, 'result_str': result_str} 
+                return result
+
+    elif (action == 'delete'):
+        payload = {
+            '_action': 'delete'
+        }
+
+    post_url = 'configuration/object/rad_nas_ip'
+
+    if (session.api_verbose == True):
+        print(f'Verbose: Sending POST to \'{session.api_url}{post_url}\' to {action} RADIUS NAS IP address')
+    
+    response = session.post(post_url, config_path, payload)
+
+    if (response.status_code == 200):
+        
+        response_json = response.json()
+        
+        if (response_json['_global_result']['status'] == 0):
+            result_str = f'{action.upper()} RADIUS NAS IP address - SUCCESS'
+            result = {'result_status': 0, 'result_str': result_str} 
+            return result
+        else:
+            result_str = f'{action.upper()} RADIUS NAS IP address - FAILED'
+            result = {'result_status': 1, 'result_str': result_str} 
+            return result
+    else:
+        result_str = f'POST to \'{session.api_url}{post_url}\' unsuccessful'
+        result = {'result_status': 1, 'result_str': result_str} 
+        return result
