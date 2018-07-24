e8b260eaf28129dfe4970e4ff8267e3b5e0dc3b1
Fisher <EFisher@BainCapital.com>
2018-07-24 08:47:36 -0400

    added copy scp flash method

--- aos8/controller/controller.py
+++ aos8/controller/controller.py
@@ -771,14 +771,14 @@ def post_snmp_server_host_snmpv3(session, config_path, action, snmp_host_ip, snm
         result = {'result_status': 1, 'result_str': result_str} 
         return result
 
-def post_copy_ftp_flash(session, config_path, ftp_host, ftp_user, ftp_password, srcfilename, destfilename):
+def post_copy_ftp_flash(session, config_path, ftp_host, ftp_user, ftp_password, srcfilename, dstfilename):
 
     payload = {
         'ftphost': ftp_host,
         'user': ftp_user,
         'passwd': ftp_password,      
         'filename': srcfilename,
-        'destfilename': destfilename
+        'destfilename': dstfilename
         }
 
     post_url = 'configuration/object/copy_ftp_flash'
@@ -863,6 +863,40 @@ def post_snmp_server_trap_source(session, config_path, action, snmp_trap_source_
             return result
         else:
             result_str = f'{action.upper()} SNMP trap source as \'{snmp_trap_source_ip}\' - FAILED'
+            result = {'result_status': 1, 'result_str': result_str} 
+            return result
+    else:
+        result_str = f'POST to \'{session.api_url}{post_url}\' unsuccessful'
+        result = {'result_status': 1, 'result_str': result_str} 
+        return result
+
+def post_copy_scp_flash(session, config_path, scp_host, scp_username, scp_password, srcfilename, dstfilename):
+
+    payload = {
+        'scphost': scp_host,
+        'username': scp_username,
+        'passwd': scp_password,       
+        'filename': srcfilename,
+        'destfilename': dstfilename,
+        }
+
+    post_url = 'configuration/object/copy_scp_flash'
+
+    if (session.api_verbose == True):
+        print(f'Verbose: Sending POST to \'{session.api_url}{post_url}\' to SCP copy {scp_host}:/{srcfilename} to flash:/{dstfilename}')
+    
+    response = session.post(post_url, config_path, payload)
+
+    if (response.status_code == 200):
+        
+        response_json = response.json()
+        
+        if (response_json['_global_result']['status'] == 0):
+            result_str = f'SCP copy {scp_host}:/{srcfilename} to flash:/{dstfilename} - SUCCESS'
+            result = {'result_status': 0, 'result_str': result_str} 
+            return result
+        else:
+            result_str = f'SCP copy {scp_host}:/{srcfilename} to flash:/{dstfilename} - FAILED'
             result = {'result_status': 1, 'result_str': result_str} 
             return result
     else:
