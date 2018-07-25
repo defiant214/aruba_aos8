69bffa9b0de3290c446b9b68c2fe7f79ac88579c
Fisher <EFisher@BainCapital.com>
2018-07-25 07:51:23 -0400

    added purge pending configuration method

--- aos8/hierarchy/hierarchy.py
+++ aos8/hierarchy/hierarchy.py
@@ -215,3 +215,34 @@ def post_configuration_device(session, node_path, action, device_model, mac_addr
         result_str = f'POST to \'{session.api_url}{post_url}\' unsuccessful'
         result = {'result_status': 1, 'result_str': result_str} 
         return result 
+
+def post_configuration_purge_pending(session, config_path):
+    
+    payload = {
+        'node-path': config_path
+    }
+
+    post_url = 'configuration/object/configuration_purge_pending'
+
+    if (session.api_verbose == True):
+        print(f'Verbose: Sending POST to \'{session.api_url}{post_url}\' to purge pending configuration')
+    
+    response = session.post(post_url, config_path, payload)
+
+    if (response.status_code == 200):
+        
+        response_json = response.json()
+        
+        if (response_json['_global_result']['status'] == 0):
+            result_str = f'Purge pending configuration - SUCCESS'
+            result = {'result_status': 0, 'result_str': result_str} 
+            return result
+        else:
+            result_str = f'Purge pending configuration - FAILED'
+            result = {'result_status': 1, 'result_str': result_str} 
+            return result
+    else:
+        result_str = f'POST to \'{session.api_url}{post_url}\' unsuccessful'
+        result = {'result_status': 1, 'result_str': result_str} 
+        return result
+
